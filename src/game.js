825303116b937723da88f84fba41f4e13cee6f07
Ayah Farhat <97855138+AyahFarhat@users.noreply.github.com>
2023-01-22 01:08:03 -0500

    Co-authored-by: Jhosim-Agudelo <Jhosim-Agudelo@users.noreply.github.com> Co-authored-by: Youssef Ouarad <youssefo7@users.noreply.github.com> Co-authored-by: RimaAz <RimaAz@users.noreply.github.com>

---
+++ src/game.js
@@ -0,0 +1,96 @@
+const EMPTY_TILE_ID = 100000;
+function fisherYatesInner(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function fisherYatesOuter(arrays) {
+  for (let i = arrays.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arrays[i], arrays[j]] = [arrays[j], arrays[i]];
+    fisherYatesInner(arrays[i]);
+  }
+}
+
+function generateArray(number, arrayPNGS) {
+  let originalArray = [];
+  let margin = 0;
+  for (let i = 0; i < number; i++) {
+    originalArray[i] = [];
+    for (let j = 0; j < number; j++) {
+      let temp=3 * i + 11 * j
+      originalArray[i][j] = { id: temp, src: arrayPNGS[margin].src };
+      // ,imgSRC:"https://images.ctfassets.net/hrltx12pl8hq/3Z1N8LpxtXNQhBD5EnIg8X/975e2497dc598bb64fde390592ae1133/spring-images-min.jpg?fit=fill&w=480&h=270"
+      margin++;
+    }
+  }
+  //console.log(originalArray)
+  originalArray[0][0].id = EMPTY_TILE_ID;
+  originalArray[0][0].src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmcthNOUKYy-64Ks-gDlFdcVksSPg--MijApkBdeU&s"
+
+  return originalArray;
+}
+function shuffleArray(number, arrayPNGS) {
+  //console.log("shuffle array");
+  let originalArray = generateArray(number, arrayPNGS);
+  let newArray = []; //=originalArray
+  for (let i = 0; i < number; i++) {
+    newArray[i] = [];
+    for (let j = 0; j < number; j++) {
+      newArray[i][j] = originalArray[i][j];
+    }
+  }
+
+  fisherYatesOuter(newArray);
+  fisherYatesInner(newArray);
+  localStorage.setItem("new array", JSON.stringify(newArray));
+  localStorage.setItem("original array", JSON.stringify(originalArray));
+  let item = localStorage.getItem("original array");
+  item = JSON.parse(item);
+}
+
+function swapTile(first, second, array) {
+  //console.log(first + " " + second + " ");
+  const temp = array[first.row][first.col].id;
+
+  array[first.row][first.col].id = array[second.row][second.col].id;
+  array[second.row][second.col].id = temp;
+}
+
+function moveTile(row, col) {
+  console.log(row + " " + col + " ");
+  let newArray = localStorage.getItem("new array");
+  newArray = JSON.parse(newArray);
+  let WhiteSpaceRow;
+  let WhiteSpaceCol;
+//console.log("tile "+row,col)
+  if (row < (newArray.length-1) && isWhiteSpace(row + 1, col, newArray)) {
+    WhiteSpaceRow = row + 1;
+    swapTile({ row, col }, { WhiteSpaceRow, col }, newArray);
+  } else if (row > 0 && isWhiteSpace(row - 1, col, newArray)) {
+    WhiteSpaceRow = row - 1;
+    swapTile({ row, col }, { WhiteSpaceRow, col }, newArray);
+  } else if (isWhiteSpace(row, col + 1, newArray) && col <( newArray.length-1)) {
+    WhiteSpaceCol = col + 1;
+    swapTile({ row, col }, { row, WhiteSpaceCol }, newArray);
+  } else if ( col >0 && isWhiteSpace(row, col - 1, newArray) ) {
+   
+    WhiteSpaceCol = col - 1;
+    swapTile({ row, col }, { row, WhiteSpaceCol }, newArray);
+  }
+}
+
+function isWhiteSpace(row, col, array) {
+ // console.log("verification "+ row,col)
+  return array[row][col].id === EMPTY_TILE_ID;
+}
+
+function checkWin() {
+  let newArray = localStorage.getItem("new array");
+  let oldArray = localStorage.getItem("original array");
+  if (newArray == oldArray) {
+    alert("you won, thanks for playing");
+  }
+}
--- src/tiles.js
+++ src/tiles.js
@@ -2,101 +2,101 @@ const preview = document.getElementById("preview");
 const gridContainer = document.getElementById("grid-container");
 
 function previewImage(event) {
-    var preview = document.getElementById("preview");
-    var file = event.target.files[0];
-    var reader = new FileReader();
+  var preview = document.getElementById("preview");
+  var file = event.target.files[0];
+  var reader = new FileReader();
 
-    reader.onloadend = function() {
+  reader.onloadend = function () {
     preview.src = reader.result;
-    }
+  };
 
-    if (file) {
+  if (file) {
     reader.readAsDataURL(file);
-    } else {
+  } else {
     preview.src = "";
-    }
+  }
 }
 
 function deleteImage(event) {
-    var image_x = document.getElementById('preview');
-    image_x.parentNode.removeChild(image_x);
+  var image_x = document.getElementById("preview");
+  image_x.parentNode.removeChild(image_x);
 }
-function handleClick(e) {
-    e.preventDefault(); //to prevent the form from submitting
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    const img = new Image();
-    img.src =  preview.src;
 
-    img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        const tileWidth = img.width / 3;
-        const tileHeight = img.height / 3;
+function handleClick(e) {
+  e.preventDefault(); //to prevent the form from submitting
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.src = preview.src;
+
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    const tileWidth = img.width / 3;
+    const tileHeight = img.height / 3;
+    const tiles = [];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const tileCanvas = document.createElement("canvas");
+        tileCanvas.width = tileWidth;
+        tileCanvas.height = tileHeight;
+        const tileCtx = tileCanvas.getContext("2d");
+        tileCtx.drawImage(
+          canvas,
+          j * tileWidth,
+          i * tileHeight,
+          tileWidth,
+          tileHeight,
+          0,
+          0,
+          tileWidth,
+          tileHeight
+        );
+        const tileImg = new Image();
+        tileImg.src = tileCanvas.toDataURL();
+        tiles.push(tileImg);
+      }
     }
+    shuffleArray(3, tiles);
+    let tileArray = localStorage.getItem("new array");
+    tileArray = JSON.parse(tileArray);
+    const grid = document.getElementById("grid-container");
+    let margin = 0;
+    const br = document.createElement("br");
+    for (let i = 0; i < 3; i++) {
+      const newRow = document.createElement("div");
+      newRow.classList.add("grid-row");
+      for (let j = 0; j < 3; j++) {
+        let img = document.createElement("img");
+        img.src = tileArray[i][j].src; //tiles[margin].src;
+        img.onclick=moveTile(i,j);
+        const newTile = document.createElement("span");
+        newTile.classList.add("tileImage");
+        newTile.setAttribute('id', 'tile' + margin.toString());
+        newTile.appendChild(img);
+        newRow.appendChild(newTile);
+        margin++;
+      }
+      grid.appendChild(newRow);
+    }
+  };
+  deleteImage(e);
 }
-
-function handleClick(e) {
-    e.preventDefault(); //to prevent the form from submitting
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    const img = new Image();
-    img.src =  preview.src;
-
-    img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        const tileWidth = img.width / 3;
-        const tileHeight = img.height / 3;
-        const tiles = [];
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                const tileCanvas = document.createElement('canvas');
-                tileCanvas.width = tileWidth;
-                tileCanvas.height = tileHeight;
-                const tileCtx = tileCanvas.getContext('2d');
-                tileCtx.drawImage(canvas, j * tileWidth, i * tileHeight, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
-                const tileImg = new Image();
-                tileImg.src = tileCanvas.toDataURL();
-                tiles.push(tileImg);
-                console.log(tileImg);
-            }
-        }
-        console.log(tiles);
-        const grid = document.getElementById('grid-container');
-        let margin = 0;
-        const br = document.createElement('br');
-        for(let i = 0; i < 3; i++)
-        {
-            const newRow = document.createElement("div");
-            newRow.classList.add("grid-row");
-            for(let j = 0; j< 3; j++){
-                let img = document.createElement("img");
-                img.src = tiles[margin].src;
-                console.log(margin)
-                const newTile = document.createElement("span");
-                newTile.classList.add("tileImage");
-                newTile.appendChild(img);
-                newRow.appendChild(newTile);
-                margin++;
-            }
-            grid.appendChild(newRow);
-        }
-    };
-    deleteImage(e);
-};
 
 //let gridItems = document.querySelectorAll(".grid-item");
 
-function shuffleArray(array) {
-    // Code to shuffle the array of image pieces
-    // ...
-}
-
-window.onload = function(){
-    const sub = document.getElementById("sub");
-    sub.addEventListener("click", handleClick);
-}
-
+window.onload = function () {
+  const sub = document.getElementById("sub");
+  sub.addEventListener("click", handleClick);
+  const tile0 = document.getElementById("tile0");
+  const tile2 = document.getElementById("tile1");
+  const tile3 = document.getElementById("tile1");
+  const tile4 = document.getElementById("tile1");
+  const tile5 = document.getElementById("tile1");
+  const tile6 = document.getElementById("tile1");
+  const tile7 = document.getElementById("tile1");
+  const tile8 = document.getElementById("tile1");
+  const tile = document.getElementById("tile1");
+  tile0.addEventListener("click", moveTile(1,1));
+};
